0dffb8ef9998b46a824f8c3a43f3a2a6dd07d1fa
Jonathan Frederick <doublej472@gmail.com>
2023-09-30 21:43:56 +0000

    Use WinSock2 and QPF for sleeping and time keeping

--- UCC/Src/ServerCommandlet.cpp
+++ UCC/Src/ServerCommandlet.cpp
@@ -1,4 +1,7 @@
+#include <winsock2.h>
 #include "../../Engine/Inc/Engine.h"
+
+#pragma comment(lib, "Ws2_32.lib")
 
 // Variables for ServerCommandlet
 
@@ -65,15 +68,22 @@ INT UServerCommandletMain(){
 	// Create input thread
 	HANDLE InputThread = CreateThread(NULL, 0, UpdateServerConsoleInput, NULL, 0, NULL);
 
-	DOUBLE OldTime = appSeconds();
-	DOUBLE SecondStartTime = OldTime;
 	INT TickCount = 0;
 
 	GIsRunning = 1;
 
+	// Use QPC for tracking time
+	LARGE_INTEGER OldTime, SecondStartTime;
+	LARGE_INTEGER Frequency;
+
+	QueryPerformanceFrequency(&Frequency);
+	QueryPerformanceCounter(&OldTime);
+	SecondStartTime.QuadPart = OldTime.QuadPart;
+
 	// Main loop
 	while(GIsRunning && !GIsRequestingExit){
-		DOUBLE NewTime = appSeconds();
+		LARGE_INTEGER NewTime;
+		QueryPerformanceCounter(&NewTime);
 
 		if(CurrentConsoleCommand){
 			if(appStricmp(CurrentConsoleCommand, "CLS") == 0) // In case user wants to clear screen. Can be useful for testing.
@@ -84,30 +94,57 @@ INT UServerCommandletMain(){
 			CurrentConsoleCommand = NULL;
 		}
 
+		LARGE_INTEGER ElapsedTime;
+		ElapsedTime.QuadPart = NewTime.QuadPart - OldTime.QuadPart;
+
+		// Calculate the deltatime in milliseconds
+		DOUBLE tickDelta = ((DOUBLE) ElapsedTime.QuadPart / (DOUBLE) Frequency.QuadPart);
+
 		// Update the world
-		GEngine->Tick(NewTime - OldTime);
+		GEngine->Tick(tickDelta);
 
-		// UEngine::Tick may load a new map and cause the timing to be reset (this is a good thing)
-		if(appSeconds() < NewTime)
-			SecondStartTime = NewTime = appSeconds();
+		//GLog->Logf("delta: %f", tickDelta);
 
-		OldTime = NewTime;
-
+		OldTime.QuadPart = NewTime.QuadPart;
 		++TickCount;
 
-		if(OldTime > SecondStartTime + 1.0){
-			GEngine->CurrentTickRate = TickCount / (OldTime - SecondStartTime);
-			SecondStartTime = OldTime;
+		// If it has been 1 second since the last update
+		if(OldTime.QuadPart > SecondStartTime.QuadPart + Frequency.QuadPart){
+			GEngine->CurrentTickRate = TickCount / ((DOUBLE) (OldTime.QuadPart - SecondStartTime.QuadPart) / (DOUBLE) Frequency.QuadPart);
+			SecondStartTime.QuadPart = OldTime.QuadPart;
 			TickCount = 0;
+			//GLog->Logf("Tickrate: %f", GEngine->CurrentTickRate);
 		}
 
 		// Enforce optional maximum tick rate
 		FLOAT MaxTickRate = GEngine->GetMaxTickRate();
 
 		if(MaxTickRate > 0.0f){
-			FLOAT Delta = (1.0f / MaxTickRate) - (appSeconds() - OldTime);
+			LARGE_INTEGER WaitTime;
+			LARGE_INTEGER TargetTime;
+			WaitTime.QuadPart = Frequency.QuadPart / MaxTickRate;
+			TargetTime.QuadPart = NewTime.QuadPart + WaitTime.QuadPart;
 
-			appSleep(Delta > 0.0f ? Delta : 0.0f);
+			// If we are waiting for over 1 milliseconds
+			// Try sleeping, leaving 1 millisecond for busy loop
+			DOUBLE WaitTimeDouble = (DOUBLE) WaitTime.QuadPart / (DOUBLE) Frequency.QuadPart;
+			if (WaitTimeDouble > 0.001) {
+				// select() somehow has the most accurate sleep, so use that
+				TIMEVAL tv;
+				tv.tv_sec = 0L;
+				tv.tv_usec = (WaitTimeDouble - 0.001) * 1000000;
+				select(NULL, NULL, NULL, NULL, &tv);
+			}
+
+			// Busy loop until next tick needs to be processed, more accurate
+			while (true) {
+				LARGE_INTEGER CurrentTime;
+				QueryPerformanceCounter(&CurrentTime);
+
+				if (CurrentTime.QuadPart >= TargetTime.QuadPart) {
+					break;
+				}
+			}
 		}
 	}
 
